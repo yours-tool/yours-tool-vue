b1f16314209df5b78d8b07a3f4668b2a0a50fcc5
wbh <136207024@qq.com>
2023-01-31 17:01:55 +0800

    添加提醒相关业务

--- src/views/Countdown.vue
+++ src/views/Countdown.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="countdown">
-    <el-card v-for="(item,index) in list" :key="index" class="box-card">
-      <div>
-        <div>类型:{{item.name}}</div>
-        <div>内容:{{item.content}}</div>
-        <div>预计花费:{{item.name}}</div>
-        <div>类型:{{item.name}}</div>
+    <div class="custom">
+      <div class="function">
+        <el-input class="input"
+            v-model="input"
+            size="small"
+            placeholder="按标签搜索"
+            suffix-icon="Search"
+        />
+        <el-button type="primary" >新增</el-button>
       </div>
-    </el-card>
+      <div class="card">
+        <div v-for="(item,index) in list" :key="index" class="single">
+          <div class="header">
+            <span>{{item.name}}</span>
+            <div>
+              <el-button class="button" size="small" type="primary" >编辑</el-button>
+              <el-button class="button" size="small" type="danger" >删除</el-button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>类型:{{item.name}}</div>
+              <div>内容:{{item.content}}</div>
+              <div>距离{{item.name}}XXX日子还有{{item.name}}天</div>
+              <div>预计花费:{{item.name}}</div>
+            </div>
+            <div>
+              <div v-for="(item,index) in list" :key="index"></div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script setup>
-
-
+import { ref } from 'vue'
+const input = ref('')
 let list = [
   {
     name: "测试11",
@@ -42,12 +70,42 @@ let list = [
 ]
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .countdown {
   width: 100%;
   height: 100vh;
   margin: 0;
   padding: 0;
   /*background-color: rgb(60, 63, 65);*/
+  .custom{
+    width: 400px;
+    .function{
+      margin: 10px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      .input{
+        width: 300px;
+      }
+    }
+
+    .card{
+      width: 400px;
+      .single{
+        margin: 10px;
+        border: 1px solid rgb(177, 179, 184);
+        border-radius: 5px;
+        .header{
+          margin: 5px 10px 5px 10px;
+
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+      }
+
+    }
+  }
 }
+
 </style>
